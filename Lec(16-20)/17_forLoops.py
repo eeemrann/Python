8c4367079002d161db4a694be849b0ab9deb299d
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2024-09-29 00:52:42 +0600

    Good Day

--- Lec(16-20)/17_forLoops.py
+++ Lec(16-20)/17_forLoops.py
@@ -19,3 +19,8 @@ for i in range (5):
     print(i)
 for k in range (5):
     print(k+1)
+for m in range (1,5):
+    print(m)    
+
+for a in range (0, 16, 3):
+    print(a)
---
+++ Lec(16-20)/18_whileLoops.py
@@ -0,0 +1,19 @@
+'''
+i=0
+while(i<=3):
+    print(i)
+    i=i+1 '''
+
+
+'''i= int(input("Enter the value of i: "))
+while(i<=4):
+    i= int(input("Enter the value of i: "))
+    print(i)'''
+
+
+count = 5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("I am inside else")    
