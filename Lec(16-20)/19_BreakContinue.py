36cc9909f2f50d7b29b0ab1d04bcdcffa2acae22
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2024-10-23 00:43:01 +0600

    its 1 am

---
+++ Lec(16-20)/19_BreakContinue.py
@@ -0,0 +1,11 @@
+for i in range(12):
+    '''print("5 X", i+1, "=", 5*(i+1))
+
+    if(i==9):
+        print("Skip the loop.")
+        break'''
+
+    if(i==9):
+        print("Skip the iteration")
+        continue
+    print("5 X", i+1, "=", 5*(i+1))        
