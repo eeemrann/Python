d7b6330467e24d0f652b37b201904c9771aef5c6
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2024-10-28 01:49:02 +0600

    1.49 AM

---
+++ Lec(21-25)/24_tuples.py
@@ -0,0 +1,20 @@
+tup = (1,2,3,"green",True)
+print(type(tup),tup)
+print(tup[-1])
+
+tup1=(1,)
+print(tup1)
+
+
+list = [1,2,3,4]
+list [0]=24
+print(list)
+
+
+if 24 in tup:
+    print("YES")
+else:
+    print("NO")    
+
+tup2=tup[1:3]
+print(tup2)
