1a9ab6ee912710557c9a817158283f3224c98fe7
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2024-11-04 23:13:09 +0600

    11.13 PM

---
+++ Lec(31-35)/31_sets.py
@@ -0,0 +1,14 @@
+s = { 2,4,2,6}
+print(s)
+
+info = {"Carla", 19, False, 5, 9, 19}
+print(info)
+
+Hello = {}
+print(type(Hello))
+
+Hello1 = set()
+print(type(Hello1))
+
+for value in info:
+    print(value)
