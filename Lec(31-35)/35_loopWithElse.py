9bafc013df61004c42d1ecf471ecd08d653967be
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2024-11-09 01:23:20 +0600

    1.23 AM

---
+++ Lec(31-35)/35_loopWithElse.py
@@ -0,0 +1,27 @@
+for i in range(6):
+    print(i)
+    if i ==4:
+      break
+else:
+  print("Sorry no i")  
+print("\n")  
+
+
+i = 0
+while i <7:
+  print(i)
+  i = i+1
+  if i == 4:
+    break
+else:
+  print("Sorry no i") 
+print("\n") 
+
+  
+for i in []:
+  print(i)
+
+else:
+  print("Sorry no i")  
+print("\n")      
+
