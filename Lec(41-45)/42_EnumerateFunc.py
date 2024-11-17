f94a60eb635df78cd6a56b9e8c5aa558f6969a85
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2024-11-18 01:26:58 +0600

    1.27 AM

---
+++ Lec(41-45)/42_EnumerateFunc.py
@@ -0,0 +1,18 @@
+marks = [12, 56, 32, 98, 12, 45, 1, 4]
+
+index=0
+for mark in marks:
+    print(mark)
+    if(index == 3):
+        print("Emran is awesome!")
+    index +=1    
+
+
+
+marksss = [112, 556, 332, 998, 126, 45, 1, 4]
+
+for index, markk in enumerate(marksss):
+    print(markk)
+    if(index == 3):
+        print("Emran is awesome!")
+    
