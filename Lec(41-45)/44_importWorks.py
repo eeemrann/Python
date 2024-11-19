59f2e46a87e412b0ac326954cf0d894a06598869
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2024-11-20 01:06:48 +0600

    1.06 AM

---
+++ Lec(41-45)/44_importWorks.py
@@ -0,0 +1,23 @@
+'''import pandas
+pandas.read_csv()'''
+
+
+'''import math
+a=math.floor(4.3356)
+print(a)
+b=math.sqrt(9)
+print(b)'''
+
+
+'''from math import sqrt,pi 
+result = sqrt(9)*pi
+print(result)'''
+
+
+'''import math as m
+c= m.sqrt(9)*m.pi
+print(c)'''
+
+
+import math
+print(dir(math))
