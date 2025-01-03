57fba7af75f0518e75760e70e5d11858116e037f
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2025-01-03 16:20:33 +0600

    4.20 PM

---
+++ Lec(56-60)/59_Decorators.py
@@ -0,0 +1,18 @@
+def greet(fx):
+    def mfx():
+        print("Good Morning")
+        fx()
+        print("Thanks for using this function")
+    return mfx
+
+
+@greet
+def hello():
+    print("Hello World")
+
+@greet
+def add(a,b):
+    print(a+b)
+
+
+hello()
